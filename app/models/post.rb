0c0a8cc50948f7bf066f11a603de3d629044551b
Cristina Fonseca <crisnf87@gmail.com>
2010-12-07 01:25:12 +0000

    Fotos no s3

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,10 +11,21 @@ class Post < ActiveRecord::Base
 	accepts_nested_attributes_for :tags, :allow_destroy => :true,
     :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 
-    has_attached_file :file, :path => "/images/:filename"
-    has_attached_file :file, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-    has_attached_file :file, :url => "/images/:filename"
+    #has_attached_file :file, :path => ":attachment/:id/:style.:extension"
+    #has_attached_file :file, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+    #has_attached_file :file, :url => ":attachment/:id/:style.:extension"
 
+	# has_attached_file :file, :storage => :s3
+	# has_attached_file :file, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml"
+	
+	
+	has_attached_file :file, 
+                                        :styles => { :medium => "300x300>", :thumb => "100x100>" },
+#                                        :url => ":s3_alias_url",                  
+                                        :storage => :s3,
+                                        :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+                                        :path => "posts/:attachment/:id/:style.:extension",
+                                        :bucket => 'actwell'
 	
 	cattr_reader :per_page
 	@@per_page = 6
